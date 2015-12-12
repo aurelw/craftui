cdc7cea04f07d0febeea7260a7a29f6d8b0168a1
Aurel Wildfellner <aurel.w@gmail.com>
2015-12-12 21:24:56 +0100

    analye_event_log.py - cluster buttons

--- scripts/analyze_event_log.py
+++ scripts/analyze_event_log.py
@@ -23,16 +23,21 @@ def readConfig(filename):
     return events
 
 
-def cluserEvents(events, minsecdist=1.0):
+def cluserEvents(events, minsecdist=1.0, segmentbuttons=False):
 
     clusters = []
     clusters.append( events[0] )
 
     ctime = events[0][0]
+    cbutton = events[0][1]
     for event in events[1:]:
         delta = event[0] - ctime
         if delta.total_seconds() > minsecdist:
             clusters.append(event)
+        if segmentbuttons and cbutton != event[1]:
+            clusters.append(event)
+            cbutton = event[1]
+
         ctime = event[0]
 
     return clusters
@@ -42,7 +47,9 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", type=str, help="Input log file.")
-    parser.add_argument("--clusterdist", type=float, default=1.0)
+    parser.add_argument("--clusterdist", type=float, default=1.0, help="Minimum distance in seconds between two event clusters.")
+    parser.add_argument("--cluster-buttons", default=False, action="store_true",
+            help="A a single cluster is only comprised out of events with the very same button.")
     args = parser.parse_args()
 
     if not args.f:
@@ -54,14 +61,9 @@ def main():
 
     events = readConfig(filename)
     print("num events: ", len(events))
-    clusters = cluserEvents(events, cluster_distance)
+    clusters = cluserEvents(events, cluster_distance, args.cluster_buttons)
     print("num clusters: ", len(clusters))
 
 
 main()
-
-
-
-
-
 
