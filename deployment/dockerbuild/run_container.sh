fed15f1ed7a7ae0d6383d0a3835a4dffa9319e18
Aurel Wildfellner <aurel.w@gmail.com>
2016-03-21 09:27:11 +0100

    Dockerbuild, mqtt, external volume,...

--- deployment/dockerbuild/run_container.sh
+++ deployment/dockerbuild/run_container.sh
@@ -2,13 +2,14 @@
 
 XAUTHORITY_FILE="/home/aurel/.Xauthority"
 
-#xhost - #disable xhost auth
+#xhost + #disable xhost auth
 
-docker run --name craftui --privileged \
+docker run --rm --name craftui --privileged \
     -v /dev/bus/usb/:/dev/bus/usb \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $XAUTHORITY_FILE:/root/.Xauthority \
-    -ti craftui/withpcl
+    -v /home/aurel/craftui_volume:/mnt/ \
+    -ti craftui 
 
-#xhost + #enable xhost auth
+#xhost - #enable xhost auth
